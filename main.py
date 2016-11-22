46eab3a374636adddc3134cd01f1b57a479cd810
Elvin Kosova <elvinkosova@gmail.com>
2016-11-22 01:51:58 +0100

    update

--- main.py
+++ main.py
@@ -9,45 +9,56 @@ stripe.api_key = "sk_test_m4QQ4tXt50ceGcWl9MsiA21B"
 
 class ListProducts(webapp2.RequestHandler):
 
-	def get(self):
-		products = stripe.Product.list(limit=3)
-		self.response.write(products)
+  def get(self):
+    self.response.headers['Content-Type'] = 'application/json'
+    data = stripe.Product.list(limit=3)
+    self.response.write(data)
 
 
 class ViewProduct(webapp2.RequestHandler):
 
-	def get(self):
-		pid = self.request.get('pid', None)
-		if pid is not None:
-			product = stripe.Product.retrieve(pid)
-			self.response.write(product)
+  def get(self, product):
+    self.response.headers['Content-Type'] = 'application/json'
+    try:
+      data = stripe.Product.retrieve(product)
+      self.response.write(data)
+    except:
+      self.response.write('')
 
 
-class Order(webapp2.RequestHandler):
+class ViewOrder(webapp2.RequestHandler):
 
-	def post(self):
-		order = self.request.get('order', None)
-		if pid is not None:
-			product = stripe.Product.retrieve(pid)
-			self.response.write(product)
+  def get(self, order):
+    self.response.headers['Content-Type'] = 'application/json'
+    try:
+      data = stripe.Order.retrieve(order)
+      self.response.write(data)
+    except:
+      self.response.write('')
+
+
+class CreateOrder(webapp2.RequestHandler):
+
+  def post(self):
+    pass
 
 
 class Pay(webapp2.RequestHandler):
 
-	def post(self):
-		order = self.request.get('order', None)
-		if pid is not None:
-			product = stripe.Product.retrieve(pid)
-			self.response.write(product)
+  def post(self):
+    pass
 
 
 class ListProductsJ(webapp2.RequestHandler):
 
-	def get(self):
-		products = stripe.Product.list(limit=3)
-		self.response.write(json.dumps(products))
+  def get(self):
+    self.response.headers['Content-Type'] = 'application/json'
+    data = stripe.Product.list(limit=3)
+    self.response.write(json.dumps(data))
 
 
-APP = webapp2.WSGIApplication([webapp2.Route(r'/', handler=ListProducts),
-															 webapp2.Route(r'/j', handler=ListProductsJ)],
+APP = webapp2.WSGIApplication([webapp2.Route(r'/products', handler=ListProducts),
+                              webapp2.Route(r'/product/<product:(.*)>', handler=ViewProduct),
+                              webapp2.Route(r'/order/<order:(.*)>', handler=ViewOrder),
+                              webapp2.Route(r'/j', handler=ListProductsJ)],
                               debug=True)
